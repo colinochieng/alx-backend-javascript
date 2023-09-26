0405572d77ccf9448f31f68ebff7410d3db41a33
colinochieng <colinokumu89@gmail.com>
2023-09-26 08:32:45 +0000

    Advanced JS

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
-  if (trueOrFalse) {
-    task = false;
-    task2 = true;
-  }
+  if (trueOrFalse) ;
 
   return [task, task2];
 }
--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: () => `$${fullBudget.income}`,
-    getIncomeInEuros: () => `${fullBudget.income} euros`,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
